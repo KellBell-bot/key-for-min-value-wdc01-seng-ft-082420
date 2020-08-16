5b854e3c51314300c87fecf2f797188d99475118
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-16 19:03:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 def key_for_min_value(name_hash)
   smallest_value = nil 
   smallest_key = nil
-  name_hash.each do |key, value|
+  name_hash.each do |item, value|
     if value > nil || value < smallest_value
       smallest_key = value
     end
